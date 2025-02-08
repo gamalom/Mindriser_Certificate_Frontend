6db575f7864caf4a3d118cdfc067959a92ce4dc8
gamalom <om.gamal10@gmail.com>
2025-02-08 19:24:27 +0545

    first commit

---
+++ routes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./src/pages/home/components/Home";
+import Register from "./src/pages/auth/register/Register";
+import Login from "./src/pages/auth/login/Login";
+
+const router = createBrowserRouter([
+  {
+    path :'/',
+    element :<Home/>
+  },
+  {
+    path :'/register',
+    element :<Register/>
+  },
+  {
+    path :'/login',
+    element :<Login/>
+  }
+])
+export default router
